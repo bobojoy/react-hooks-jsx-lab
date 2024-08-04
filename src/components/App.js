b34b8e825bddd491b5ae2650ccabf7ba91043c95
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-08-04 22:17:35 +0300

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
+import { name as UserName, city as City, image as Image } from "../data/data";
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
       <About />
+      <UserName />
+      <City />
+      <Image />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name as username, city } from "../data/data";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {username} is a Web Developer from {city}
+      </h1>
+      <p></p>
+    </div>
+  );
 }
 
 export default Home;
